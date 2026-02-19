c5050d58fcdff7899e80577675589b8f43923811
Luca Sainenco <lucasainenco2@gmail.com>
2026-02-19 17:30:30 +0100

    dev: test push

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260214_135919 from './20260214_135919';
+import * as migration_20260219_161936 from './20260219_161936';
 
 export const migrations = [
   {
     up: migration_20260214_135919.up,
     down: migration_20260214_135919.down,
-    name: '20260214_135919'
+    name: '20260214_135919',
+  },
+  {
+    up: migration_20260219_161936.up,
+    down: migration_20260219_161936.down,
+    name: '20260219_161936'
   },
 ];
